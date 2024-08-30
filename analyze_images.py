89a314eed713d52bbd0b7d7bef7d265f98918db4
TIMOTHY ANDES <tim.andes+git@gmail.com>
2024-08-30 13:17:53 -0600

    commit functioning MVP

--- analyze_images.py
+++ analyze_images.py
@@ -10,8 +10,8 @@ from PIL import Image
 load_dotenv()
 
 # instantiating the Bedrock client, and passing in the CLI profile
-boto3.setup_default_session(profile_name=os.getenv("profile_name"))
-bedrock = boto3.client('bedrock-runtime', 'us-east-1', endpoint_url='https://bedrock-runtime.us-west-2.amazonaws.com')
+boto3.setup_default_session(profile_name=os.getenv("default"))
+bedrock = boto3.client('bedrock-runtime', 'us-east-1', endpoint_url='https://bedrock-runtime.us-east-1.amazonaws.com')
 
 
 def image_base64_encoder(image_name):
